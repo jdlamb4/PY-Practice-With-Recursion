5484a7019fc76c42668b7f3649213bedf86ef16d
Jacob Lamb <jacoblamb15@gmail.com>
2022-09-13 06:19:31 -0400

    added another solution to gcd bonus

--- Bonus.py
+++ Bonus.py
@@ -8,3 +8,13 @@ def gcd(x,y):
 
 print("gcd of 15 and 10:",gcd(15,10))
 print("gcd of 46 and 12:",gcd(46,12))
+
+def gcd(a,b): 
+    print(f"a{a} -- b{b}") 
+    if b==0: 
+        print(f"a{a} -- b{b}") 
+        return a 
+    else: 
+        print(f"a{a} -- b{b}-- a%b{a%b}") 
+        return gcd(b,a%b)
+gcd(15,20)
