1d8cd361f2d361752daf04baf3a1cff7728f4099
Jacob Lamb <jacoblamb15@gmail.com>
2022-09-13 06:07:33 -0400

    updated solution 1

--- solution1.py
+++ solution1.py
@@ -1,11 +1,12 @@
-# Write code for algorithm 1 below
+# 1.  Write a program that takes a positive number as an argument and prints the numbers from n to zero.
+
 def count_down(n):
 
-  #  Base case
+  # Base case
   if n==0:
       return
 
-  #  Recursive case
+  # Recursive case
   else:
       print(n)
       count_down(n-1)
